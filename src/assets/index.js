5c7c5152c6d746fb2fbef232b01c5e3c8a667c43
PythoSalaf <alfaheed1010@gmail.com>
2025-01-08 09:39:33 +0100

    error issues fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,19 +5,5 @@ import Project2 from "./ECT.svg";
 import Project3 from "./vtu.png";
 import Project4 from "./teddy.svg";
 import ContactBg from "./contact.svg";
-import Isreal from "./isreal.svg";
-import Preshi from "./preshi.svg";
-import Taoheed from "./taoheed.svg";
 
-export {
-  Logo,
-  Hero,
-  Project1,
-  Project2,
-  Project3,
-  Project4,
-  ContactBg,
-  Isreal,
-  Preshi,
-  Taoheed,
-};
+export { Logo, Hero, Project1, Project2, Project3, Project4, ContactBg };
